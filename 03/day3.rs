dd217c99b68beee24a275be6c7fac092c00c75a4
Sjoerd Dost <deosjr@gmail.com>
2019-12-03 17:16:04 +0100

    updated day 3 with line segment logic instead of hashmaps

--- 03/day3.rs
+++ 03/day3.rs
@@ -1,107 +1,93 @@
 use std::fs::File;
 use std::io; use std::io::prelude::*;
 use std::str::FromStr;
-use std::collections::HashMap;
 
-fn parse(filename: &str) -> io::Result<(String, String)> {
+#[derive(Debug, Clone, Copy)]
+struct Segment {
+    fixed_axis: bool, // true for X / false for Y
+    distance_start: i32,
+    fixed: i32,
+    begin: i32,
+    end: i32
+}
+
+fn parse(filename: &str) -> io::Result<(Vec<Segment>,Vec<Segment>)> {
     let file = File::open(filename)?;
     let mut lines = io::BufReader::new(file).lines(); let wire1 = lines.next().unwrap()?;
     let wire2 = lines.next().unwrap()?;
-    Ok((wire1, wire2))
+    Ok((wire_to_segments(wire1.trim_end()), wire_to_segments(wire2.trim_end())))
 }
 
-fn map1(wire: String) -> HashMap<(i32,i32),i32> {
-    let mut m = HashMap::new();
-    m.insert((0,0), 0);
-    let instrs = wire.trim_end().split(',');
+fn wire_to_segments(wire: &str) -> Vec<Segment> {
+    let instrs = wire.split(',');
     let mut x = 0;
     let mut y = 0;
     let mut distance = 0;
+    let mut v: Vec<Segment> = Vec::new();
     for s in instrs {
         let mut n = String::from(s);
         let instr: String =  n.drain(..1).collect();
         let z = i32::from_str(&*n).unwrap();
         match instr.chars().next().unwrap() {
-           'U' => {
-                for yy in y..y+z {
-                    distance+=1;
-                    let coord = (x, yy+1);
-                    match m.get(&coord) {
-                        Some(d) => (),//distance=*d,
-                        None => {m.insert(coord, distance); ()}
-                    }
-                }
-                y = y+z;
-               }, 
-           'R' => {
-                for xx in x..x+z {
-                    distance+=1;
-                    let coord = (xx+1, y);
-                    match m.get(&coord) {
-                        Some(d) => (),//distance=*d,
-                        None => {m.insert(coord, distance); ()}
-                    }
-                }
-                x = x+z;
-               }, 
-           'D' => {
-                for yy in (y-z+1..y+1).rev() {
-                    distance+=1;
-                    let coord = (x, yy-1);
-                    match m.get(&coord) {
-                        Some(d) => (),//distance=*d,
-                        None => {m.insert(coord, distance); ()}
-                    }
-                }
-                y = y-z;
-               }, 
-           'L' => {
-                for xx in (x-z+1..x+1).rev() {
-                    distance+=1;
-                    let coord = (xx-1, y);
-                    match m.get(&coord) {
-                        Some(d) => (),//distance=*d,
-                        None => {m.insert(coord, distance); ()}
-                    }
-                }
-                x = x-z;
-               }, 
+           'U' => {v.push(Segment{fixed_axis:true, distance_start:distance, fixed:x, begin:y, end:y+z}); y=y+z}, 
+           'R' => {v.push(Segment{fixed_axis:false, distance_start:distance, fixed:y, begin:x, end:x+z}); x=x+z}, 
+           'D' => {v.push(Segment{fixed_axis:true, distance_start:distance, fixed:x, begin:y, end:y-z}); y=y-z}, 
+           'L' => {v.push(Segment{fixed_axis:false, distance_start:distance, fixed:y, begin:x, end:x-z}); x=x-z}, 
            _ => panic!()
         }
+        distance += z;
     }
-    m
+    v
+}
+
+fn collision(w: Segment, v: Segment, result_func:fn(Segment,Segment)->i32) -> i32 {
+    if w.fixed_axis == v.fixed_axis {
+        if w.fixed == v.fixed {
+            // overlap on same line?
+        }
+        return -1 
+    }
+    if ((w.begin < v.fixed && w.end > v.fixed) || (w.end < v.fixed && w.begin > v.fixed)) 
+        && ((v.begin < w.fixed && v.end > w.fixed) || (v.end < w.fixed && v.begin > w.fixed)) {
+        return result_func(w, v)
+    }
+    -1
+}
+
+fn result1(w: Segment, v: Segment) -> i32 {
+    w.fixed.abs() + v.fixed.abs()
+}
+
+fn part1(m1: &Vec<Segment>, m2: &Vec<Segment>) -> i32 {
+    main_loop(m1, m2, result1)
+}
+
+fn result2(w: Segment, v: Segment) -> i32 {
+    w.distance_start + (v.fixed - w.begin).abs() + v.distance_start + (w.fixed - v.begin).abs()
+}
+
+fn part2(m1: &Vec<Segment>, m2: &Vec<Segment>) -> i32 {
+    main_loop(m1, m2, result2)
+}
+
+fn main_loop(m1: &Vec<Segment>, m2: &Vec<Segment>, result_func:fn(Segment,Segment)->i32) -> i32 {
+    let mut delay = -1;
+    for w in m1 {
+        for v in m2 {
+            let c = collision(*w,*v,result_func);
+            if c == -1 || c == 0 {
+                continue; 
+            }
+            if delay == -1 || c < delay {
+                delay = c;
+            }
+        }
+    }
+    delay
 }
 
 fn main() {
     let (wire1, wire2) = parse("day3.input").unwrap();
-    let m1 = map1(wire1);
-    let m2 = map1(wire2);
-    let mut manhattan = 0;
-    for key in m1.keys() {
-       let (x, y) = key; 
-       if *x==0 && *y==0 {
-            continue
-       }
-       if m2.contains_key(key) {
-            let m = x.abs() + y.abs();
-            if manhattan == 0 || m < manhattan {
-                manhattan = m
-            }
-       }
-    }
-    println!("Part 1: {:?}", manhattan);
-    let mut delay = 0;
-    for key in m1.keys() {
-       let (x, y) = key; 
-       if *x==0 && *y==0 {
-            continue
-       }
-       if m2.contains_key(key) {
-            let d = m1.get(key).unwrap() + m2.get(key).unwrap();
-            if delay == 0 || d < delay {
-                delay = d
-            }
-       }
-    }
-    println!("Part 2: {:?}", delay);
+    println!("Part 1: {}", part1(&wire1, &wire2));
+    println!("Part 2: {}", part2(&wire1, &wire2));
 }
