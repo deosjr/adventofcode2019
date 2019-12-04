befaf4e6706034aa9bc990bc16d6c2d559ff0b4d
Sjoerd Dost <deosjr@gmail.com>
2019-12-04 12:11:44 +0100

    fixed prolog TODO

--- 04/day4.pl
+++ 04/day4.pl
@@ -20,13 +20,13 @@ password2(Pass) :-
     labeling([ff],P),
     Pass #= 100000 * A + 10000 * B + 1000 * C + 100 * D + 10 * E + F,
     \+(all_distinct(P)),
-    global_cardinality(P, [1-R,2-S,3-T,4-U,5-V,6-W,7-X,8-Y,9-Z]),
-    (R#=2;S#=2;T#=2;U#=2;V#=2;W#=2;X#=2;Y#=2;Z#=2).
+    Cardinalities = [1-_,2-_,3-_,4-_,5-_,6-_,7-_,8-_,9-_],
+    global_cardinality(P, Cardinalities),
+    selectchk(_-2, Cardinalities, _).
 
 part2(N) :-
     P in 307237..769058,
-    % TODO: why are we finding doubles and I have to filter them out with setof?
-    setof(P, password2(P), Passwords),
+    bagof(P, password2(P), Passwords),
     length(Passwords, N).
 
 run :-
