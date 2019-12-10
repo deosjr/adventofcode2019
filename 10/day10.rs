3c9fd61a984cc9c6b3861091dacb8328981f7341
Sjoerd Dost <deosjr@gmail.com>
2019-12-10 11:10:23 +0100

    day 10 part 2 done in rust

--- 10/day10.rs
+++ 10/day10.rs
@@ -1,7 +1,26 @@
 use std::fs::File;
 use std::io;
-use std::io::prelude::*;
-use std::collections::HashMap;
+use std::io::prelude::*; use std::collections::HashMap;
+
+#[derive(PartialEq,Eq,Hash,Debug,Clone,Copy)]
+struct Coord {
+    x: i32,
+    y: i32
+}
+
+impl Coord {
+    fn new(x: i32, y: i32) -> Coord {
+        Coord{x:x, y:y}
+    }
+
+    fn length(self) -> f64 {
+        ((self.x*self.x + self.y*self.y) as f64).sqrt()
+    }
+
+    fn unit_y(self) -> f64 {
+        self.y as f64 / self.length()
+    }
+}
 
 fn read_file(filename: &str) -> io::Result<io::BufReader<File>> {
     let file = File::open(filename)?;
@@ -9,13 +28,13 @@ fn read_file(filename: &str) -> io::Result<io::BufReader<File>> {
     Ok(reader)
 }
 
-fn parse(reader: io::BufReader<File>) -> HashMap<(i32,i32), i32> {
+fn parse(reader: io::BufReader<File>) -> HashMap<Coord, i32> {
     let mut m = HashMap::new();
     for (y, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         for (x, c) in line.chars().enumerate() {
             match c {
-                '#' => m.insert((x as i32, y as i32), 0),
+                '#' => m.insert(Coord::new(x as i32, y as i32), 0),
                 '.' => continue,
                 _   => panic!()
             };
@@ -32,12 +51,11 @@ fn gcd(x: i32, y: i32) -> i32 {
     }
 }
 
-fn collision(m: &HashMap<(i32,i32),i32>, from: (i32,i32), to: (i32,i32), steps: (i32,i32)) -> bool {
+fn collision(m: &HashMap<Coord,i32>, from: Coord, to: Coord, steps: Coord) -> bool {
     let mut coords = from;
     loop {
-        let (x, y) = coords;
-        let (dx, dy) = steps;
-        coords = (x+dx, y+dy);
+        coords.x += steps.x;
+        coords.y += steps.y;
         if coords == to {
             return false
         }
@@ -52,25 +70,59 @@ fn main() {
     let reader = read_file("day10.input").unwrap();
     let m = parse(reader);
     let mut max = 0;
-    for (from, _) in &m {
+    let mut station = Coord::new(-1, -1);
+    for (&from, _) in m.iter() {
         let mut detected = 0;
-        for (to, _) in &m {
+        for (&to, _) in m.iter() {
             if from == to {
                 continue
             }
-            let (x, y) = from;
-            let (x2, y2) = to;
-            let (diffx, diffy) = (x2-x, y2-y);
-            let steps = gcd(diffx, diffy);
-            let (diffx, diffy) = (diffx/steps, diffy/steps);
-            if collision(&m, *from, *to, (diffx, diffy)) {
+            let mut diff = Coord::new(to.x-from.x, to.y-from.y);
+            let steps = gcd(diff.x, diff.y);
+            diff.x = diff.x/steps;
+            diff.y = diff.y/steps;
+            if collision(&m, from, to, diff) {
                 continue
             }
             detected += 1;
         }
         if max < detected {
             max = detected;
+            station = from;
         }
     }
     println!("Part 1: {}", max);
+    let mut order: HashMap<i32, Vec<Coord>> = HashMap::new();
+    for (&asteroid, _) in m.iter() {
+        if asteroid == station {
+            continue
+        }
+        let relative = Coord::new(asteroid.x - station.x, asteroid.y - station.y);
+        let acos = relative.unit_y().acos();
+        let o = if relative.x >= 0 { acos  } else {  -acos  };
+        if let Some(v) = order.get_mut(&((o*10000.) as i32)) {
+            v.push(relative);
+            v.sort_by(|a, b| (a.x.abs()+a.y.abs()).cmp(&(b.x.abs()+b.y.abs())).reverse());
+        } else {
+            order.insert((o*10000.) as i32, vec![relative]);
+        }
+    }
+
+    let mut keys = order.keys().map(|&x| x).collect::<Vec<i32>>();
+    keys.sort();
+    keys.reverse();
+    let mut i = 0;
+    for k in keys.iter().cycle() {
+        let v = order.get_mut(&k).unwrap();
+        if v.len() == 0 {
+            continue
+        }
+        let asteroid = v.pop().unwrap();
+        i += 1;
+        if i == 200 {
+            println!("Part 2: {}", (asteroid.x + station.x) * 100 + asteroid.y + station.y);
+            break
+        }
+    }
+
 }
