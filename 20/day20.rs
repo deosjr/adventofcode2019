a6dc569c4f27fe73c78dfb00f454128106b6e0e9
Sjoerd Dost <deosjr@gmail.com>
2019-12-30 11:34:02 +0100

    generic pathfinding lib in rust

--- 20/day20.rs
+++ 20/day20.rs
@@ -2,57 +2,38 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::collections::HashMap;
-use std::collections::BinaryHeap;
+
+mod path;
 
 fn parse(filename: &str) -> Vec<String> {
     let file = File::open(filename).expect("");
     let reader = io::BufReader::new(file);
     reader.lines().map(|l| l.unwrap()).collect::<Vec<String>>() }
 
-#[derive(Debug,Ord,PartialOrd,Eq,PartialEq)]
-struct PQItem {
-    fscore: i64,
-    node: Node
-}
-
-impl PQItem {
-    fn new(node: Node, fscore: i64) -> PQItem {
-        PQItem{
-            fscore: fscore,
-            node:   node,
-        }
-    }
-}
-
 #[derive(Debug,Ord,PartialOrd,Eq,PartialEq,Hash,Copy,Clone)]
-struct Node {
+struct NodeP1 {
     x: i64,
     y: i64
 }
 
-impl Node {
-    fn new(x: i64, y: i64) -> Node {
-        Node{x: x, y: y}
+impl NodeP1 {
+    fn new(x: i64, y: i64) -> NodeP1 {
+        NodeP1{x: x, y: y}
     }
 }
 
-trait Map {
-    fn neighbours(&self, n: Node) -> Vec<Node>;
-    fn g(&self, n: Node, neighbour: Node) -> i64;
-    fn h(&self, n: Node, goal: Node) -> i64;
-}
-
 struct Donut {
-    coords: HashMap<Node,Tile>
+    coords: HashMap<NodeP1,Tile>
 }
 
-impl Map for Donut {
-    fn neighbours(&self, n: Node) -> Vec<Node> {
-        let mut neighbours: Vec<Node> = vec![];
-        let npoints = [ Node::new(n.x + 1, n.y),
-                        Node::new(n.x - 1, n.y),
-                        Node::new(n.x, n.y + 1),
-                        Node::new(n.x, n.y - 1) ];
+impl path::Map for Donut {
+    type Node = NodeP1; 
+    fn neighbours(&self, n: Self::Node) -> Vec<Self::Node> {
+        let mut neighbours: Vec<Self::Node> = vec![];
+        let npoints = [ NodeP1::new(n.x + 1, n.y),
+                        NodeP1::new(n.x - 1, n.y),
+                        NodeP1::new(n.x, n.y + 1),
+                        NodeP1::new(n.x, n.y - 1) ];
         for p in npoints.iter() {
             match self.coords.get(&p) {
                 Some(t) => {
@@ -68,105 +49,31 @@ impl Map for Donut {
         neighbours
     }
 
-    fn g(&self, _n: Node, _neighbour: Node) -> i64 { 1 }
+    fn g(&self, _n: Self::Node, _neighbour: Self::Node) -> i64 { 1 }
 
-    fn h(&self, n: Node, goal: Node) -> i64 {
+    fn h(&self, n: Self::Node, goal: Self::Node) -> i64 {
         let dx = goal.x - n.x;
         let dy = goal.y - n.y;
         dx.abs() + dy.abs()
     }
 }
 
-#[derive(Clone)]
 struct Tile {
-    point: Node,
-    warp:  Node
+    point: NodeP1,
+    warp:  NodeP1
 }
 
 impl Tile {
-    fn new(p: Node) -> Tile {
-        Tile{point: p, warp: Node::new(0,0)}
+    fn new(p: NodeP1) -> Tile {
+        Tile{point: p, warp: NodeP1::new(0,0)}
     }
 }
 
-fn find_route<T: Map>(map: T, start: Node, goal: Node) -> Option<Vec<Node>>{
-    let mut open_set: HashMap<Node,bool> = HashMap::new();
-    open_set.insert(start, true);
-
-    let mut came_from: HashMap<Node,Node> = HashMap::new();
-
-    let mut g_score: HashMap<Node,i64> = HashMap::new();
-    g_score.insert(start, 0);
-
-    let mut f_score: HashMap<Node,i64> = HashMap::new();
-    let start_h = map.h(start, goal);
-    f_score.insert(start, start_h);
-
-    let mut pq = BinaryHeap::new();
-    pq.push(PQItem::new(start, start_h));
-
-    let mut goal_score = std::i64::MAX;
-
-    while !pq.is_empty() {
-        let item = pq.pop().unwrap();
-        if item.fscore == std::i64::MAX {
-            break
-        }
-        let current = item.node;
-        let current_gscore = *(g_score.get(&current).unwrap());
-        if current == goal {
-            goal_score = current_gscore;
-        }
-        open_set.remove(&current);
-
-        for n in map.neighbours(current) {
-            let tentative_gscore = current_gscore + map.g(current, n);
-            let f = tentative_gscore + map.h(n, goal);
-
-            let interesting_f = match f_score.get(&n) {
-                None => true,
-                Some(&v) => f < v
-            };
-
-            if !open_set.contains_key(&n) && interesting_f && f < goal_score {
-                open_set.insert(n, true);
-                pq.push(PQItem::new(n, f));
-            } else if tentative_gscore >= *(g_score.get(&n).unwrap()) {
-                continue
-            }
-            came_from.insert(n, current);
-            g_score.insert(n, tentative_gscore);
-            f_score.insert(n, f);
-        }
-    }
-    if goal_score == std::i64::MAX {
-        None
-    } else {
-        Some(reconstruct_path(came_from, goal))
-    }
-}
-
-fn reconstruct_path(came_from: HashMap<Node,Node>, mut current: Node) -> Vec<Node> {
-    let mut path = vec![current];
-    loop {
-        match came_from.get(&current) {
-            None => break,
-            Some(&prev) => {
-                current = prev;
-                path.push(current);
-            }
-        }
-    }
-    path
-}
-
-fn connect_portals(m: &mut HashMap<Node,Tile>, labels: &mut HashMap<String,Node>, label: String, p1: Node) {
+fn connect_portals(m: &mut HashMap<NodeP1,Tile>, labels: &mut HashMap<String,NodeP1>, label: String, p1: NodeP1) {
     match labels.get(&label) {
         Some(&p2) => {
-            let t1 = m.get(&p1).unwrap().clone();
-            let t2 = m.get(&p2).unwrap().clone();
-            m.insert(p1, Tile{point: t1.point, warp: p2});
-            m.insert(p2, Tile{point: t2.point, warp: p1});
+            m.insert(p1, Tile{point: p1, warp: p2});
+            m.insert(p2, Tile{point: p2, warp: p1});
         },
         None => {labels.insert(label, p1); ()}
     }
@@ -193,45 +100,52 @@ fn main() {
     let innertop = outertop + yoff - 1;
     let innerright = outerright - yoff + 1;
     let innerbottom = outerbottom - yoff + 1;
-
-    let mut m: HashMap<Node,Tile> = HashMap::new();
-    let mut labels: HashMap<String,Node> = HashMap::new();
+    
+    let mut m: HashMap<NodeP1,Tile> = HashMap::new();
+    let mut labels: HashMap<String,NodeP1> = HashMap::new();
     for (y, line) in grid.iter().enumerate() {
         for (x, r) in line.chars().enumerate() {
             if r == '#' || r == ' ' {
                 continue
             }
-            let p = Node::new(x as i64, y as i64);
+            let p = NodeP1::new(x as i64, y as i64);
             if r == '.' {
                 m.insert(p, Tile::new(p));
             }
-            if y > innertop && y < innerbottom && (x == outerleft || x == innerright) {
-                // read portal label to the left
-                let l1 = grid.get(y).unwrap().chars().nth(x-2).unwrap();
-                let l2 = grid.get(y).unwrap().chars().nth(x-1).unwrap();
-                let label = format!("{}{}", l1, l2);
-                connect_portals(&mut m, &mut labels, label, p);
+            if y > innertop && y < innerbottom {
+                if x == outerleft || x == innerright {
+                    // read portal label to the left
+                    let l1 = grid.get(y).unwrap().chars().nth(x-2).unwrap();
+                    let l2 = grid.get(y).unwrap().chars().nth(x-1).unwrap();
+                    let label = format!("{}{}", l1, l2);
+                    connect_portals(&mut m, &mut labels, label, p);
+                    continue
+                }
+                if x == outerright || x == innerleft {
+                    // read portal label to the right
+                    let l1 = grid.get(y).unwrap().chars().nth(x+1).unwrap();
+                    let l2 = grid.get(y).unwrap().chars().nth(x+2).unwrap();
+                    let label = format!("{}{}", l1, l2);
+                    connect_portals(&mut m, &mut labels, label, p);
+                    continue
+                }
             }
-            if x > innerleft && x < innerright && (y == outertop || y == innerbottom) {
-                // read portal label above
-                let l1 = grid.get(y-2).unwrap().chars().nth(x).unwrap();
-                let l2 = grid.get(y-1).unwrap().chars().nth(x).unwrap();
-                let label = format!("{}{}", l1, l2);
-                connect_portals(&mut m, &mut labels, label, p);
-            }
-            if y > innertop && y < innerbottom && (x == outerright || x == innerleft) {
-                // read portal label to the right
-                let l1 = grid.get(y).unwrap().chars().nth(x+1).unwrap();
-                let l2 = grid.get(y).unwrap().chars().nth(x+2).unwrap();
-                let label = format!("{}{}", l1, l2);
-                connect_portals(&mut m, &mut labels, label, p);
-            }
-            if x > innerleft && x < innerright && (y == outerbottom || y == innertop) {
-                // read portal label below
-                let l1 = grid.get(y+1).unwrap().chars().nth(x).unwrap();
-                let l2 = grid.get(y+2).unwrap().chars().nth(x).unwrap();
-                let label = format!("{}{}", l1, l2);
-                connect_portals(&mut m, &mut labels, label, p);
+            if x > innerleft && x < innerright {
+                if y == outertop || y == innerbottom {
+                    // read portal label above
+                    let l1 = grid.get(y-2).unwrap().chars().nth(x).unwrap();
+                    let l2 = grid.get(y-1).unwrap().chars().nth(x).unwrap();
+                    let label = format!("{}{}", l1, l2);
+                    connect_portals(&mut m, &mut labels, label, p);
+                    continue
+                }
+                if y == outerbottom || y == innertop {
+                    // read portal label below
+                    let l1 = grid.get(y+1).unwrap().chars().nth(x).unwrap();
+                    let l2 = grid.get(y+2).unwrap().chars().nth(x).unwrap();
+                    let label = format!("{}{}", l1, l2);
+                    connect_portals(&mut m, &mut labels, label, p);
+                }
             }
         }
     }
@@ -241,5 +155,5 @@ fn main() {
     let start = labels.get("AA").unwrap();
     let end = labels.get("ZZ").unwrap();
 
-    println!("Part 1: {}", find_route(dm, *start, *end).unwrap().len() - 1);
+    println!("Part 1: {}", path::find_route(dm, *start, *end).unwrap().len() - 1);
 }
---
+++ 20/path.rs
@@ -0,0 +1,100 @@
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+
+pub trait Map {
+    type Node;
+    fn neighbours(&self, n: Self::Node) -> Vec<Self::Node>;
+    fn g(&self, n: Self::Node, neighbour: Self::Node) -> i64;
+    fn h(&self, n: Self::Node, goal: Self::Node) -> i64;
+}
+
+#[derive(Debug,Ord,PartialOrd,Eq,PartialEq)]
+struct PQItem<N> {
+    fscore: i64,
+    node: N
+}
+
+impl<N> PQItem<N> {
+    fn new(node: N, fscore: i64) -> PQItem<N> {
+        PQItem{
+            fscore: fscore,
+            node:   node,
+        }
+    }
+}
+
+pub fn find_route<T,N>(map: T, start: N, goal: N) -> Option<Vec<N>>
+    where T: Map<Node=N>, N: Copy + Ord + std::hash::Hash
+    {
+    let mut open_set: HashMap<N,bool> = HashMap::new();
+    open_set.insert(start, true);
+
+    let mut came_from: HashMap<N,N> = HashMap::new();
+
+    let mut g_score: HashMap<N,i64> = HashMap::new();
+    g_score.insert(start, 0);
+
+    let mut f_score: HashMap<N,i64> = HashMap::new();
+    let start_h = map.h(start, goal);
+    f_score.insert(start, start_h);
+
+    let mut pq = BinaryHeap::new();
+    pq.push(PQItem::new(start, start_h));
+
+    let mut goal_score = std::i64::MAX;
+
+    while !pq.is_empty() {
+        let item = pq.pop().unwrap();
+        if item.fscore == std::i64::MAX {
+            break
+        }
+        let current = item.node;
+        let current_gscore = *(g_score.get(&current).unwrap());
+        if current == goal {
+            goal_score = current_gscore;
+        }
+        open_set.remove(&current);
+
+        for n in map.neighbours(current) {
+            let tentative_gscore = current_gscore + map.g(current, n);
+            let f = tentative_gscore + map.h(n, goal);
+
+            let interesting_f = match f_score.get(&n) {
+                None => true,
+                Some(&v) => f < v
+            };
+
+            if !open_set.contains_key(&n) && interesting_f && f < goal_score {
+                open_set.insert(n, true);
+                pq.push(PQItem::new(n, f));
+            } else if tentative_gscore >= *(g_score.get(&n).unwrap()) {
+                continue
+            }
+            came_from.insert(n, current);
+            g_score.insert(n, tentative_gscore);
+            f_score.insert(n, f);
+        }
+    }
+    if goal_score == std::i64::MAX {
+        None
+    } else {
+        Some(reconstruct_path(came_from, goal))
+    }
+}
+
+fn reconstruct_path<N>(came_from: HashMap<N,N>, mut current: N) -> Vec<N>
+    where N: Eq + std::hash::Hash + Copy
+    {
+    let mut path = vec![current];
+    loop {
+        match came_from.get(&current) {
+            None => break,
+            Some(&prev) => {
+                current = prev;
+                path.push(current);
+            }
+        }
+    }
+    path
+}
+
